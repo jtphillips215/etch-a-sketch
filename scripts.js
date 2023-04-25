54ca7e45c9761414ba2592c87aaa81a81dff9258
jtphillips215 <jtphillips215@gmail.com>
2023-04-25 14:05:14 -0500

    added skeleton to html, beginning javascript to add cells to device

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// creating a reference to the etch a sketch
+const device = document.querySelector("device");
+
+// create grid for etch a sketch
+function createGrid(numOfCells) {
+  for (let i = 0; i < numOfCells; i++) {
+    // creating rowns
+  }
+  for (let i = 0; i < numOfCells; i++) {
+    // creating cells in those rows
+  }
+  return;
+}
