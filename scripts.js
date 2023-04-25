94d04631e97f5fa78509c4c9d9b4c0ddccd3bcf2
jtphillips215 <jtphillips215@gmail.com>
2023-04-25 14:12:42 -0500

    beggining work on js to generate cells on device

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@ const device = document.querySelector("device");
 // create grid for etch a sketch
 function createGrid(numOfCells) {
   for (let i = 0; i < numOfCells; i++) {
-    // creating rowns
+    const row = document.createElement("div");
+    // maybe nested loop would be better to generate cells?
+    device.appendChild(row);
   }
   for (let i = 0; i < numOfCells; i++) {
     // creating cells in those rows
