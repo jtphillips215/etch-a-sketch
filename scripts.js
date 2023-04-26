26ab400aa93efa526545da5074e8ecfded405f29
jtphillips215 <jtphillips215@gmail.com>
2023-04-26 11:38:28 -0500

    updated styling and uncommented code to generate cells

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 "use strict";
 
-/*
 // creating a reference to the etch a sketch
-const device = document.querySelector("#device");
+const screen = document.querySelector("#screen");
 
 // create grid for etch a sketch
 function createGrid(numOfCells) {
@@ -10,12 +9,13 @@ function createGrid(numOfCells) {
     // creating element and adding an id and class
     const row = document.createElement("div");
     row.classList.add("row");
-    for (let j = 0; j < numOfCells; j++) {
+    for (let j = 0; j < numOfCells * 1.5; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
       row.appendChild(cell);
     }
-    device.appendChild(row);
+    screen.appendChild(row);
   }
 }
-*/
+
+createGrid(16);
