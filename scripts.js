e59ed50b0b80a7e9bb781eecdd18664e7606aa7e
jtphillips215 <jtphillips215@gmail.com>
2023-04-25 20:17:10 -0500

    updated function to generate cells, added some styling

--- scripts.js
+++ scripts.js
@@ -1,20 +1,21 @@
 "use strict";
 
 // creating a reference to the etch a sketch
-const device = document.querySelector("device");
+const device = document.querySelector("#device");
 
 // create grid for etch a sketch
 function createGrid(numOfCells) {
   for (let i = 0; i < numOfCells; i++) {
     // creating element and adding an id and class
     const row = document.createElement("div");
-    row.setAttribute("class", "row");
+    row.classList.add("row");
     for (let j = 0; j < numOfCells; j++) {
-      const cell = document.createElement("cell");
-      cell.setAttribute("class", "cell");
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
       row.appendChild(cell);
     }
     device.appendChild(row);
   }
-  return;
 }
+
+createGrid(16);
